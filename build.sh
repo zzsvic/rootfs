c487bcb7052f0cad7a93ec395c62b633f6acaf0b
zhou.zhaosong <772264113@qq.com>
2020-09-13 18:02:56 +0800

    提交说明：创建busybox1.32.0 linux根文件系统版本库

---
+++ build.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name     : build.sh
+# Author        : zhou.zhaosong
+# mail          : zzsvic@163.com
+# Created Time  : 2020年09月13日 星期日 18时00分40秒
+#########################################################################
+
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+
